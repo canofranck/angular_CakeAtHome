5dcd5d0e3dee1fe82508ae7059c347fc4567697f
CANOFRANCK <cano.franck77@gmail.com>
2023-01-23 14:57:35 +0100

    avec omponents header footer

--- CakeAtHome/src/app/app.module.ts
+++ CakeAtHome/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecetteComponent } from './components/recette/recette.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LastrecipesComponent } from './components/lastrecipes/lastrecipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecetteComponent
+    RecetteComponent,
+    HeaderComponent,
+    FooterComponent,
+    LastrecipesComponent
   ],
   imports: [
     BrowserModule,
